5e5984bef647ee393b0abfde9e8eea5a1f04f657
Karlo Bratko <kbratko@racunarstvo.hr>
2022-06-21 17:33:11 +0200

    fix: update create procedures and recreation process
    Create procedures can recreate rows if there is deleted row that has every unique column.

--- Apartments.DB/Programmability/TagTypes/CRUD/Create.sql
+++ Apartments.DB/Programmability/TagTypes/CRUD/Create.sql
@@ -46,29 +46,44 @@ AS BEGIN
   END
   ELSE IF @Guid       IS NOT NULL AND 
           @DeleteDate IS NOT NULL BEGIN
-    UPDATE [dbo].[TagTypes]
-    SET
-      [DeleteDate] = NULL,
-      [DeletedBy]  = NULL,
-      [UpdateDate] = GETDATE(),
-      [UpdatedBy]  = @CreatedBy
-    WHERE [Guid] = @Guid
+    SET @Guid = NULL
+    SET @Guid = (
+      SELECT ALL TOP 1
+        [Guid]
+      FROM [dbo].[TagTypes]
+      WHERE [Name]    = @Name AND
+            [NameEng] = @NameEng
+    )
+    IF @Guid IS NOT NULL BEGIN
+      UPDATE [dbo].[TagTypes]
+      SET
+        [DeleteDate] = NULL,
+        [DeletedBy]  = NULL,
+        [UpdateDate] = GETDATE(),
+        [UpdatedBy]  = @CreatedBy,
+        [Name]       = @Name,
+        [NameEng]    = @NameEng
+      WHERE [Guid] = @Guid
 
-    SELECT ALL
-      [Id],
-      [Guid],
-      [CreateDate],
-      [CreatedBy],
-      [UpdateDate],
-      [UpdatedBy],
-      [DeleteDate],
-      [DeletedBy],
-      [Name],
-      [NameEng]
-    FROM [dbo].[TagTypes]
-    WHERE [Guid] = @Guid
+      SELECT ALL
+        [Id],
+        [Guid],
+        [CreateDate],
+        [CreatedBy],
+        [UpdateDate],
+        [UpdatedBy],
+        [DeleteDate],
+        [DeletedBy],
+        [Name],
+        [NameEng]
+      FROM [dbo].[TagTypes]
+      WHERE [Guid] = @Guid
 
-    RETURN 3
+      RETURN 3
+    END
+    ELSE BEGIN
+      RETURN 2
+    END
   END
   ELSE IF @Guid       IS NOT NULL AND 
           @DeleteDate IS NULL BEGIN
