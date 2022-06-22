585a6c13d5c8a0de14a23540eb8843216edd6fd6
Karlo Bratko <kbratko@racunarstvo.hr>
2022-06-22 10:05:29 +0200

    feat: ad CRUD procedures for Reviews DB table
    Add CRUD procedures for Reviews DB table. Procedures return status codes.

---
+++ Apartments.DB/Programmability/Reviews/CRUD/Create.sql
@@ -0,0 +1,117 @@
+﻿CREATE PROCEDURE [dbo].[ReviewCreate] (@ApartmentFK AS int,
+                                       @UserFK      AS int,
+                                       @Details     AS nvarchar(500),
+                                       @Stars       AS int,
+                                       @CreatedBy   AS int = 1)
+AS BEGIN
+  DECLARE @Guid       AS uniqueidentifier
+  DECLARE @DeleteDate AS datetime
+  SELECT ALL TOP 1
+    @Guid       = [Guid],
+    @DeleteDate = [DeleteDate]
+  FROM [dbo].[Reviews]
+  WHERE [ApartmentFK] = @ApartmentFK AND
+        [UserFK]     = @UserFK
+
+  IF @Guid IS NULL BEGIN
+    INSERT INTO [dbo].[Reviews]
+    (
+      [CreatedBy],
+      [UpdatedBy],
+      [ApartmentFK],
+      [UserFK],
+      [Details],
+      [Stars]
+    )
+    VALUES
+    (
+      @CreatedBy,
+      @CreatedBy,
+      @ApartmentFK,
+      @UserFK,
+      @Details,
+      @Stars
+    )
+
+    DECLARE @Id AS int = SCOPE_IDENTITY()
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [UserFK],
+      [Details],
+      [Stars]
+    FROM [dbo].[Reviews]
+    WHERE [Id] = @Id
+
+    RETURN 1
+  END
+  ELSE IF @Guid       IS NOT NULL AND 
+          @DeleteDate IS NOT NULL BEGIN
+    UPDATE [dbo].[Reviews]
+    SET
+      [DeleteDate]  = NULL,
+      [DeletedBy]   = NULL,
+      [UpdateDate]  = GETDATE(),
+      [UpdatedBy]   = @CreatedBy,
+      [Details]     = @DeleteDate,
+      [Stars]       = @Stars
+    WHERE [Guid] = @Guid
+
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [UserFK],
+      [Details],
+      [Stars]
+    FROM [dbo].[Reviews]
+    WHERE [Guid] = @Guid
+
+    RETURN 3
+  END
+  ELSE IF @Guid       IS NOT NULL AND 
+          @DeleteDate IS NULL BEGIN
+    UPDATE [dbo].[Reviews]
+    SET
+      [UpdateDate] = GETDATE(),
+      [UpdatedBy]  = @CreatedBy,
+      [Details]     = @DeleteDate,
+      [Stars]       = @Stars
+    WHERE [Guid] = @Guid
+
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [UserFK],
+      [Details],
+      [Stars]
+    FROM [dbo].[Reviews]
+    WHERE [Guid] = @Guid
+
+    RETURN 2
+  END
+  ELSE BEGIN
+    RETURN -1
+  END
+END
+GO
---
+++ Apartments.DB/Programmability/Reviews/CRUD/Read.sql
@@ -0,0 +1,73 @@
+﻿CREATE PROCEDURE [dbo].[ReviewRead] (@Method AS int,
+                                     @Guid   AS uniqueidentifier = NULL)
+AS BEGIN
+IF @Method = 1 BEGIN
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [UserFK],
+      [Details],
+      [Stars]
+    FROM [dbo].[Reviews]
+  END
+  ELSE IF @Method = 2 BEGIN
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [UserFK],
+      [Details],
+      [Stars]
+    FROM [dbo].[Reviews]
+    WHERE [DeleteDate] IS NULL
+  END
+  ELSE IF @Method = 3 BEGIN
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [UserFK],
+      [Details],
+      [Stars]
+    FROM [dbo].[Reviews]
+    WHERE [Guid] = @Guid
+  END
+  ELSE IF @Method = 4 BEGIN
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [UserFK],
+      [Details],
+      [Stars]
+    FROM [dbo].[Reviews]
+    WHERE [DeleteDate] IS NULL AND
+          [Guid] = @Guid
+  END
+END
+GO
