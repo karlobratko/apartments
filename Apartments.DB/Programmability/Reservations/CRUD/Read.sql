abb9fb984970e5c7c851cd31fb89f2dd8430268e
Karlo Bratko <kbratko@racunarstvo.hr>
2022-06-22 09:18:40 +0200

    feat: add CRUD procedures for Reservations DB table
    Add CRUD procedures for Reservations DB table. Procedures return status codes.
    Update Reservations table, remove [UserName], add [UserFName] and [UserLName] columns.

---
+++ Apartments.DB/Programmability/Reservations/CRUD/Read.sql
@@ -0,0 +1,89 @@
+﻿CREATE PROCEDURE [dbo].[ReservationsRead] (@Method AS int,
+                                           @Guid   AS uniqueidentifier = NULL)
+AS BEGIN
+  IF @Method = 1 BEGIN
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [Details],
+      [UserFK],
+      [UserFName],
+      [UserLName],
+      [UserEmail],
+      [UserPhoneNumber],
+      [UserAddress]
+    FROM [dbo].[Reservations]
+  END
+  ELSE IF @Method = 2 BEGIN
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [Details],
+      [UserFK],
+      [UserFName],
+      [UserLName],
+      [UserEmail],
+      [UserPhoneNumber],
+      [UserAddress]
+    FROM [dbo].[Reservations]
+    WHERE [DeleteDate] IS NULL
+  END
+  ELSE IF @Method = 3 BEGIN
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [Details],
+      [UserFK],
+      [UserFName],
+      [UserLName],
+      [UserEmail],
+      [UserPhoneNumber],
+      [UserAddress]
+    FROM [dbo].[Reservations]
+    WHERE [Guid] = @Guid
+  END
+  ELSE IF @Method = 4 BEGIN
+    SELECT ALL
+      [Id],
+      [Guid],
+      [CreateDate],
+      [CreatedBy],
+      [UpdateDate],
+      [UpdatedBy],
+      [DeleteDate],
+      [DeletedBy],
+      [ApartmentFK],
+      [Details],
+      [UserFK],
+      [UserFName],
+      [UserLName],
+      [UserEmail],
+      [UserPhoneNumber],
+      [UserAddress]
+    FROM [dbo].[Reservations]
+    WHERE [DeleteDate] IS NULL AND
+          [Guid] = @Guid
+  END
+END
+GO
